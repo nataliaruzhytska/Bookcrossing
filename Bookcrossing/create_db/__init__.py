8aafecdcb5cea178fd739250430dbc95839753e3
natalia.ruzhytska@synevo.ua <natalia.ruzhytska@synevo.ua>
2020-02-10 22:30:45 +0200

    delete folder

--- Bookcrossing/create_db/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from flask_restful import Api
-from create_db.routes import CreateDB
-
-create_db = Blueprint("create_db", __name__)
-api_create_db = Api(create_db)
-
-api_create_db.add_resource(CreateDB, "/create_db")
